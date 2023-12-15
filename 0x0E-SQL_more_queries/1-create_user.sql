00635fce6df7863fd0dc4a1de3356d5acc433c4c
Godfred <goddysmart2468@gmail.com>
2023-12-14 21:04:26 -0800

    creates the MySQL server user user_0d_1

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,4 @@
+-- this creates the MySQL server user user_0d_1 granting all priviledges
+CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON * . * TO user_0d_1@localhost;
+
