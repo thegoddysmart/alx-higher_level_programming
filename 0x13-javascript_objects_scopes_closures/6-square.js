6852b9e8bf1e34dd2e5e1335eaccf0ff7598cc50
Godfred <goddysmart2468@gmail.com>
2024-01-09 11:26:48 -0800

    Square #1

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+const SquarePrev = require('./5-square');
+
+class Square extends SquarePrev {
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      let s = '';
+      for (let j = 0; j < this.width; j++) {
+        s += c;
+      }
+      console.log(s);
+    }
+  }
+}
+
+module.exports = Square;
