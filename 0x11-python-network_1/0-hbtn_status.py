d3475f0b8f7c3bc7b7dbcb148ece4d5671e263e6
Godfred <goddysmart2468@gmail.com>
2024-03-05 19:56:22 -0800

    What my status? #0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# A Python script that fetches a url
+
+import urllib.request
+
+url = 'https://alx-intranet.hbtn.io/status'
+
+with urllib.request.urlopen(url) as response:
+    body = response.read()
+
+print("- Body response:")
+print("\t- type:", type(body))
+print("\t- content:", body)
+print("\t- utf8 content:", body.decode('utf-8'))
